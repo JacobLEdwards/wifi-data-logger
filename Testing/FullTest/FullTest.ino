ff2a1616b8c552acfbd46b4aa5ecac4cb4e70f12
JacobLEdwards <JacobEdwards60@Googlemail.com>
2019-08-10 19:16:39 +0100

    Full Functionality Test, and Final Schematic

--- Testing/FullTest/FullTest.ino
+++ Testing/FullTest/FullTest.ino
@@ -19,13 +19,14 @@ void setup() {
  
   Serial.begin(115200);                            //Serial connection
   WiFi.begin(ssid, password);   //WiFi connection
- 
+
+ Serial.println("");
+ Serial.println("Connecting.");
   while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
- 
     delay(500);
-    Serial.println("Waiting for connection");
- 
+    Serial.print(".");
   }
+  Serial.println("");
 
   // Initialise Sensor
   Serial.println("Initialising Sensor.");
@@ -34,43 +35,52 @@ void setup() {
   dht.temperature().getSensor(&sensor);
   dht.humidity().getSensor(&sensor);
   delay(2000);
+  Serial.println("Startup Successful.");
 }
  
 void loop() {
  // Check WiFi
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
     // Setup JSON payload.
-    StaticJsonDocument<100> testDocument;
-
+    StaticJsonDocument<100> postDoc;
 
     // Get Data.
     // Temp.
     sensors_event_t event;
     dht.temperature().getEvent(&event);
     if (isnan(event.temperature)) {
-      testDocument["Temp"] = "ERROR";
+      postDoc["Temp"] = "ERROR";
     }
     else {
-      testDocument["Temp"] = event.temperature;
+      postDoc["Temp"] = event.temperature;
+      Serial.println("Temperature Read Successful");
     }
 
     // Humidity
     dht.humidity().getEvent(&event);
     if (isnan(event.relative_humidity)) {
-      testDocument["Humi"] = "ERROR";
+      postDoc["Humi"] = "ERROR";
     }
     else {
-      testDocument["Humi"] = event.relative_humidity;
+      postDoc["Humi"] = event.relative_humidity;
+      Serial.println("Humidity Read Successful");
     }
 
     // Battery Voltage.
     sensorValue = analogRead(vReadPin);
-    sensorValue = map(sensorValue, 200, 400, 0, 100);
-    testDocument["Batt"] = sensorValue;
+    sensorValue = map(sensorValue, 250, 500, 0, 100);
+    postDoc["Batt"] = sensorValue;
+    Serial.println("Battery Voltage Read Successful");
+    // End Logging if battery voltage low.
+    if (sensorValue < 10)
+    {
+      Serial.println("BATTERY LOW! Entering sleep mode.");
+      ESP.deepSleep(0);
+    }
     
     // Serialize JSON.
     char buffer[100];
-    serializeJson(testDocument, buffer);
+    serializeJson(postDoc, buffer);
     Serial.println(buffer);
 
     // Setup Request
@@ -94,6 +104,6 @@ void loop() {
  
   }
  
-  ESP.deepSleep(10e6);  //Send a request every 1 minute
+  ESP.deepSleep(3e8);  //Send a request every 5 mins.
  
 }
